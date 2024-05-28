4c734a0e677600e230ad3ba940f582d48e03d14c
pipzgutz <pmgutierrez@pm.me>
2024-05-28 21:48:36 +0800

    init

---
+++ src/main/java/net/groundgurus/level2/spring_web_mvc_example/SpringWebMvcExampleApplication.java
@@ -0,0 +1,13 @@
+package net.groundgurus.level2.spring_web_mvc_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvcExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMvcExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/groundgurus/level2/spring_web_mvc_example/SpringWebMvcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.groundgurus.level2.spring_web_mvc_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebMvcExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
